d575e8e5ef8fde7aa964429f893d94be20bc31af
eldor9313 <akhmadjonoveldorbek@gmail.com>
2025-09-26 23:55:58 +0900

    BRR: Initial commit Anadoli nest project

---
+++ libs/enums/notification.enum.ts
@@ -0,0 +1,15 @@
+export enum NotificationType {
+	LIKE = 'LIKE',
+	COMMENT = 'COMMENT',
+}
+
+export enum NotificationStatus {
+	UNREAD = 'UNREAD',
+	READ = 'READ',
+}
+
+export enum NotificationGroup {
+	MEMBER = 'MEMBER',
+	ARTICLE = 'ARTICLE',
+	PROPERTY = 'PROPERTY',
+}
---
+++ libs/enums/property.enum.ts
@@ -0,0 +1,23 @@
+export enum PropertyType {
+	HOTEL = 'HOTEL',
+	HISTORICAL = 'HISTORICAL',
+	RESORT = 'RESORT',
+}
+
+export enum PropertyStatus {
+	ACTIVE = 'ACTIVE',
+	SOLD = 'SOLD',
+	DELETE = 'DELETE',
+}
+
+export enum PropertyLocation {
+	ISTANBUL = 'ISTANBUL',
+	ANTALYA = 'ANTALYA',
+	MUGLA = 'MUGLA',
+	IZMIR = 'IZMIR',
+	ADANA = 'ADANA',
+	MARMARIS = 'MARMARIS',
+	ANKARA = 'ANKARA',
+	BODRUM = 'BODRUM',
+	CAPPADOCIA = 'CAPPADOCIA',
+}
